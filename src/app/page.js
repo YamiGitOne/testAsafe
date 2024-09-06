f26bc53af3aec0d2c079d9134d49b65c9ad1fe33
Yamilet Jerez <99609403+YamiGitOne@users.noreply.github.com>
2024-09-06 14:06:54 +0200

    -

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default async function Home() {
       </main>
       <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">mi carrito Yamii - Todos los derechos Reservados</p>
+            <p className="text-white text-center fs-4 mt-4 m-md-0">Todos los derechos Reservados</p>
         </div>
     </footer>
     </div>
